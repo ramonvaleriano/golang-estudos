7e61d5877d6db8bc72922bb9f65d74e3d754fb35
Ramon Valeriano <rrvaleriano@gmail.com>
2025-02-12 15:02:13 -0300

    Entendendo conceitos de variáveis

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	fmt.Println("Olá mundo, aprendendo Golang.")
@@ -15,4 +18,39 @@ func main() {
 	fmt.Println("Estamos usando na versão: ", versao_string)
 	fmt.Println("Versão em float: ", versao_float)
 	fmt.Println("A idade é: ", idade)
+
+	// Em muitos casos você não precisa declarar qual o tipo, o Go saberá interpretar qual o tipo de cada variável.
+	var nome_usuario = "Valeriano"
+	var idade_nova = 35
+	var versao_nova = 1.2
+
+	fmt.Println("O seu nome é: ", nome_usuario, "Tem a idade: ", idade_nova)
+	fmt.Println("A versão: ", versao_nova)
+
+	// Para saber quais o tipos de variáveis.
+	fmt.Println("Tipo do nome: ", reflect.TypeOf(nome_usuario))
+	fmt.Println("Tipo da idade: ", reflect.TypeOf(idade_nova))
+	fmt.Println("Tipo de versão: ", reflect.TypeOf(versao_nova))
+
+	// Atribuição de variaveis curto, no lugar de declarar "var"
+
+	nome_dela := "Milla"
+	idade_dela := 36
+
+	fmt.Println("O nome dela: ", nome_dela, "A idade dela: ", idade_dela)
+
+	fmt.Println("Olá Sr. ", nome_do_usuario)
+	fmt.Println("O programa está na versão: ", versao_float)
+
+	fmt.Println("Menu: ")
+	fmt.Println("1 - Iniciar Monitoramento.")
+	fmt.Println("2 - Exibir Logs.")
+	fmt.Println("3 - Sair do Programa.")
+
+	comando := 0
+
+	fmt.Scan(&comando)
+
+	println("O comando digitado foi: ", comando)
+	println("O ponteiro, o endereço da variável comando é: ", &comando)
 }
